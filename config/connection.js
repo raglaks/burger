11636c8dd1572156e7e0ff473cca4c85be547d3e
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-09 13:03:21 -0600

    add connection.js file; connect to mysql db

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+
+});
+
+connection.connect(function (err) {
+
+    if (err) {
+
+        console.error("Error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("Connected as id " + connection.threadId);
+
+});
+
+module.exports = connection;
