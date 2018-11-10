c8ffd3f26d6465b635d017eec46a4accdd1a3352
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-11-09 19:14:17 -0600

    add methods to burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,12 +2,32 @@ const orm = require("../config/orm");
 
 const burger = {
 
-    allBurgers: function (tableName) {
+    allBurgers: function () {
 
-        orm.selectAll(tableName);
+        orm.selectAll("burgers");
+
+    },
+
+    newBurger: function (name, devoured) {
+
+        orm.insertOne("burgers", "burger_name", "devoured", name, devoured);
+
+        console.log("\nSuccessfully added.\n");
+
+    },
+
+    editBurger: function (col1, val1, idVal) {
+
+        orm.updateOne("burgers", col1, val1, idVal);
+
+        console.log("\nSuccessfully updated.\n");
 
     }
 
 }
+
+//burger.newBurger("el muy macho", false);
+
+burger.editBurger("burger_name", "rated r", 7);
 
 burger.allBurgers("burgers");
